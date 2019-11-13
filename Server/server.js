2f6f29972ebbf24fe90f8143aebf8d19c17575e6
Arjun Banerjee <ab5518@ic.ac.uk>
2019-11-13 00:35:57 +0000

    Implemented basic API functionality

---
+++ Server/server.js
@@ -0,0 +1,65 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+const request = require('request');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' });
+});
+// more routes for our API will happen here
+
+// get searched subreddit top stories
+router.route('/search/:subreddit')
+// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res) {
+      let query = req.params.subreddit;
+      let url = `https://www.reddit.com/r/${query}/top.json`;
+      request(url, function (err, response, body) {
+          if(err) {
+              res.json("Not available");
+          } else {
+              res.json(JSON.parse(body));
+          }
+      });
+    });
+
+// get searched subreddit top stories
+router.route('/search/:subreddit/year')
+// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res) {
+        let query = req.params.subreddit;
+        let url = `https://www.reddit.com/r/${query}/top.json`;
+        request(url, function (err, response, body) {
+            if(err) {
+                res.status(err.httpRequestStatusCode).send("Error");
+            } else {
+                res.json(JSON.parse(body));
+            }
+        });
+    });
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
