ecc3de4f1359cdb8dc340de4e74a6bce4f7df379
Arjun Banerjee <ab5518@ic.ac.uk>
2019-11-15 14:45:10 +0000

    Started to develop front end created basic structure and DTOs for interacting with backend service.

---
+++ Frontend/src/app/component/subreddit-post/subreddit-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subreddit-post',
+  templateUrl: './subreddit-post.component.html',
+  styleUrls: ['./subreddit-post.component.scss']
+})
+export class SubredditPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
